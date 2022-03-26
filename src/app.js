372db353f8edeb42c6cd4313d2cddd0b0eca3a90
elphaba787 <elphaba787@yahoo.com>
2022-03-26 10:14:34 -0400

    Finished Layout CSS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
+let cityInput = "New York";
+let apiKey = "58852fb69388463e1271f80d4fc7e8f6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 function cityInput() {}
