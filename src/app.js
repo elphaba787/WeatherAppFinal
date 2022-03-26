76acbe3ac4afd578bbdd206a4726c2b34959be64
elphaba787 <elphaba787@yahoo.com>
2022-03-26 12:41:27 -0400

    Added Formatting for Degrees

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
-let cityInput = "New York";
+function displayInfomation(response) {
+  let cityElement = document.querySelector("#city-name");
+  let tempElement = document.querySelector("#current-temp");
+  cityElement.innerHTML = response.data.name;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  console.log(response);
+}
+let cityName = "Tampa";
 let apiKey = "58852fb69388463e1271f80d4fc7e8f6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
-function cityInput() {}
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayInfomation);
