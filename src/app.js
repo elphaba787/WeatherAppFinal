7fc84c0108d5ae7281e44b8881dfe90519ec6c33
elphaba787 <elphaba787@yahoo.com>
2022-03-27 12:18:01 -0400

    Added the functions for Celcius and Fahrenheit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ function displayInfomation(response) {
   let descriptionElement = document.querySelector("#description");
   let iconElement = document.querySelector("#icon");
 
+  celciusTemp = response.data.main.temp;
   cityElement.innerHTML = response.data.name;
   humidElement.innerHTML = response.data.main.humidity;
   iconElement.setAttribute(
@@ -15,8 +16,7 @@ function displayInfomation(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
   descriptionElement.innerHTML = response.data.weather[0].description;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  tempElement.innerHTML = Math.round(response.data.main.temp);
-  console.log(response);
+  tempElement.innerHTML = Math.round(celciusTemp);
 }
 
 function cityButton(event) {
@@ -28,10 +28,35 @@ function cityButton(event) {
   cityHeading.innerHTML = cityName.value;
 }
 
+function changeToCelcius(event) {
+  event.preventDefault();
+
+  let changeCelcius = (celciusTemp * 9) / 5 + 32;
+  let tempElement = document.querySelector("#current-temp");
+
+  tempElement.innerHTML = Math.round(changeCelcius);
+}
+
+function changeToFahrenheit(event) {
+  event.preventDefault();
+
+  let tempElement = document.querySelector("#current-temp");
+
+  tempElement.innerHTML = Math.round(celciusTemp);
+}
+
 let form = document.querySelector("#city-input");
 form.addEventListener("submit", cityButton);
 
+let fahrenheitChange = document.querySelector("#fahrenheit");
+fahrenheitChange.addEventListener("click", changeToCelcius);
+
+let celciusChange = document.querySelector("#celcius");
+celciusChange.addEventListener("click", changeToFahrenheit);
+
 let cityName = "Tampa";
+let celciusTemp = null;
+let fahrenheitTemp = null;
 let apiKey = "58852fb69388463e1271f80d4fc7e8f6";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
